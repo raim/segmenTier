a0a14a7cc3245f817252e260153541083cd4fb7f
Rainer Machne <raim@tbi.univie.ac.at>
2016-11-01 20:19:01 +0100

    more comments on segment_data and plotting to R instead of png

--- demo/segment_data.R
+++ demo/segment_data.R
@@ -11,7 +11,7 @@ data(primseg436)
 use.fft <- TRUE # cluster discrete Fourier transform of data?
 use.snr <- TRUE # take SNR of DFT
 trafo <- "" # "ash" # "log" # 
-low.thresh <- 1 # -Inf/0 # filter by log(DC-component=total signal over time)
+low.thresh <- 1 # -Inf/0 # minimal mean value (DC component of DFT if use.fft)
 dft.range <- 2:7 # range of DFT to cluster to use for clustering
 selected <- c(8,12,16) # cluster number K
 iter.max <- 100000 # max. iterations in kmeans
@@ -38,24 +38,29 @@ multib <- "max" # c("max","skip","min")
 nextmax <-TRUE
 
 
+## PRE-PROCESS TIME SERIES FOR CLUSTERING
+## take DFT and scale amplitudes, and
+## select components of DFT
+
 tset <- processTimeseries(ts=tsd, smooth=FALSE, trafo=trafo,
                           use.fft=use.fft, dft.range=dft.range,
                           use.snr=use.snr, low.thresh=low.thresh)
 
-
+## CLUSTER PRE-PROCESS TIME SERIES
 cset <- clusterTimeseries(tset, selected=selected,iter.max=iter.max, nstart=nstart)
 
+## CALCULATE SEGMENTS FOR ALL CLUSTERINGS and
+## FOR CHOSEN SEGMENTATION PARAMETERS
 allsegs <- segmentCluster.batch(cset, csim.scale=csim.scale, score=scores,
                                 M=M, Mn=Mn, a=2, nui=nui.cr,
                                 nextmax=nextmax, multi=multi,multib=multib, 
                                 ncpu=1, verb=1, save.mat="")
 
+## PLOT RESULTS
 
-    ## plot all
-#out <- file.path(paste(outname,"_",segid,sep=""))
-#coors <- c(chr=1,unlist(primseg[i,1:2])) 
-#plotdev(out,width=6,height=3.5,type=fig.type)
 
+## TODO: convert genomeBrowser to library
+## or include simplified versions of the plot in segmenTier
 browser.path <- sub("GENBRO=","",system("env|grep GENBRO",intern=TRUE))
 source(file.path(browser.path,"src/genomeBrowser_utils.R")) # for coor2index
 source(file.path(browser.path,"src/genomeBrowser.R")) ## for chrS
@@ -73,7 +78,7 @@ colors0 <- matlab.like(100)  ## read count
 colors0[1] <- "#FFFFFF" ## replace minimal by white
 
 
-png("testset_srg1_scales.png",width=10,height=5,units="in",res=300)
+##png("testset_srg1_scales.png",width=10,height=5,units="in",res=300)
 par(mfcol=c(3,1),mai=c(.01,1.5,.01,.01),mgp=c(1.7,.5,0),xaxs="i")
 plot(1:N,tot,log=ifelse(trafo!="","","y"),type="l",lwd=2,axes=FALSE)
 points((1:N)[low],tot[low],col=2,cex=.5)
@@ -83,4 +88,4 @@ columns <- c(name="ID", type="type", start="start", end="end")
 tmp <- plotFeatures(allsegs, coors=coors,
                     typord=TRUE,cuttypes=TRUE,
                     ylab="", names=FALSE,columns=columns,tcx=.5)
-dev.off()
+##dev.off()
