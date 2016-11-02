b70874e3a6cda40080ddc96989f5679b0c498213
Rainer Machne <raim@tbi.univie.ac.at>
2016-11-02 23:57:45 +0100

    added shadowtext from genomeBrowser

--- R/plot.R
+++ R/plot.R
@@ -3,6 +3,20 @@
 
 ### PLOTTING RESULTS - from genomeBrowser 20161102
 
+## from lib TeachingDemos; used in plotFeatures
+shadowtext <- function(x, y=NULL, labels, col='white', bg='black',
+                       theta= seq(pi/4, 2*pi, length.out=8), r=0.1, ... ) {
+  
+  xy <- xy.coords(x,y)
+  xo <- r*strwidth('A')
+  yo <- r*strheight('A')
+  
+  for (i in theta) {
+    text( xy$x + cos(i)*xo, xy$y + sin(i)*yo, labels, col=bg, ... )
+  }
+  text(xy$x, xy$y, labels, col=col, ... )
+}
+
 #' plot features as arrows
 #'@export
 segment.plotFeatures <- function(data, coors, types, strand,
