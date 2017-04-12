036bb6bd9883df4f402fc6f233d66424225df6f6
raim <raim@tbi.univie.ac.at>
2017-04-12 17:16:23 +0200

    added alternative combos for best

--- demo/segment_data.R
+++ demo/segment_data.R
@@ -9,12 +9,12 @@ if ( debug ) {
     sourceCpp("~/programs/segmenTier/src/cluster.cpp")
     load("~/programs/segmenTier/data/primseg436.rda")
 } else {
+
+    ## load library
     library("segmenTier")
-
-
-## load time-series data
-## contains tsd from primseg436 for
-## a 7.6 kb genomic region
+    ## load time-series data
+    ## contains tsd from primseg436 for
+    ## a 7.6 kb genomic region
     data(primseg436)
 }
 
@@ -119,7 +119,18 @@ vary <- setVarySettings(
 )
 bad2 <- segmentCluster.batch(cset, varySettings=vary,type.name=c("E","M","nui"))
 
-## BEST FRAGMENATION - cyan
+## BEST FRAGMENATIONS - cyan
+
+## best 1 - high E/nui - long M
+vary <- setVarySettings(
+    E=3,    # scale exponent of similarity matrices csim
+    S="icor", # SCORING FUNCTIONS
+    M=200,   # scoring function minimal length penalty
+    Mn=100,   # M for nuissance clusters
+    nui=3   #-/+ correlation of nuissance cluster with others and itself
+)
+best1 <- segmentCluster.batch(cset, varySettings=vary,type.name=c("E","M","nui"))
+## best 2 - intermediate
 vary <- setVarySettings(
     E=2,    # scale exponent of similarity matrices csim
     S="icor", # SCORING FUNCTIONS
@@ -127,13 +138,22 @@ vary <- setVarySettings(
     Mn=100,   # M for nuissance clusters
     nui=2   #-/+ correlation of nuissance cluster with others and itself
 )
-best <- segmentCluster.batch(cset, varySettings=vary,type.name=c("E","M","nui"))
+best2 <- segmentCluster.batch(cset, varySettings=vary,type.name=c("E","M","nui"))
+## best 3 - E/nui=1 - short M
+vary <- setVarySettings(
+    E=1,    # scale exponent of similarity matrices csim
+    S="icor", # SCORING FUNCTIONS
+    M=75,   # scoring function minimal length penalty
+    Mn=100,   # M for nuissance clusters
+    nui=1   #-/+ correlation of nuissance cluster with others and itself
+)
+best3 <- segmentCluster.batch(cset, varySettings=vary,type.name=c("E","M","nui"))
 
 ## use layout to combine plots
 if ( plot.pdf )
-  plotdev("segment_data_examples",res=300,width=10,height=4.5,type="pdf")
+  plotdev("segment_data_examples",res=300,width=10,height=5,type="pdf")
 #layout(matrix(1:9,ncol=1),heights=c(.25,.5,.5,.3,.3,.3,.1,.1,.1))
-layout(matrix(1:6,ncol=1),heights=c(.25,.5,.5,.1,.1,.1))
+layout(matrix(1:8,ncol=1),heights=c(.25,.5,.5,.075,.075,.075,.075,.075))
 par(mai=c(0.1,2,0.05,0.01),xaxs="i",yaxs="r")
 par(cex=1) 
 plot(tset,ylabh=TRUE)
@@ -142,7 +162,9 @@ plot(cset,axes=2,cex=.7)
 par(cex=1.2) # increase axis labels
 par(mai=c(0.01,2,0.01,0.01))
 plot(bad1,"segments",lwd=3)
-plot(best,"segments",lwd=3)
+plot(best3,"segments",lwd=3)
+plot(best2,"segments",lwd=3)
+plot(best1,"segments",lwd=3)
 plot(bad2,"segments",lwd=3)
 if ( plot.pdf )
   dev.off()
