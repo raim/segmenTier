7568b562d6914ef9b4c45affaabebd590d6bd29b
raim <raim@tbi.univie.ac.at>
2019-01-31 09:59:58 +0100

    slight changes for better use

--- generateRpackage.R
+++ generateRpackage.R
@@ -1,22 +1,21 @@
 
 pkg <- "~/programs/segmenTier/pkg"
 
-
-
 ## generate documentation build/check package
-library(devtools)
-
 Rcpp::compileAttributes(pkg)
-document(pkg) # 
-check(pkg,cran=TRUE) # check if all is OK
-build(pkg) # generate .tar.gz
+devtools::document(pkg) # 
+devtools::check(pkg,cran=TRUE) # check if all is OK
+devtools::build(pkg) # generate .tar.gz
 
 library(goodpractice)
 g <- gp(pkg)
 
 
 ## generate vignette
+pkg <- "~/programs/segmenTier/pkg"
 rmarkdown::render(file.path(pkg,"vignettes","segmenTier.Rmd"))
 # generate doc pdf in folder 
 ## DOESNT WORK: check(pkg=pkg, path=pkg, args=c('--no-examples'), manual=TRUE)
+
+pkg <- "~/programs/segmenTier/pkg"
 system(paste("rm pkg.pdf -f; R CMD Rd2pdf", pkg))
